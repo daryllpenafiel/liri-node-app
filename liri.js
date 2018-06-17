f318e679881657656daf040c2510f326d4bbbbe8
daryllpenafiel <daryll_penafiel@yahoo.com>
2018-06-16 23:43:44 -0400

    yay

---
+++ liri.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+var userChoice = process.argv[2];
+
+switch(userChoice){
+    case "my-tweets":
+        //code 
+        break;
+    case "spotify-this song":
+        //code
+        break;
+    case "movie-this":
+        //code
+        break;
+    case "do-what-it-says":
+        //code
+        break;
+}
+
